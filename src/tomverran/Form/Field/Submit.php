7fc5adbae94276224a7319311253375c68c09cc1
tom <javaguychronox@gmail.com>
2014-03-14 19:57:48 +0000

    add submit field

---
+++ src/tomverran/Form/Field/Submit.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Submit.php
+ * @author Tom
+ * @since 14/03/14
+ */
+
+namespace tomverran\Form\Field;
+use tomverran\Form\Input;
+
+class Submit extends Input
+{
+    protected $type = 'submit';
+} 
